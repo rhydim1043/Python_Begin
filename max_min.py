2cc7a0c12701c1a076f6a1916584b86c32fe6472
RhydimGoel <rhydim@sourcewiz.co>
2023-12-05 15:26:04 +0530

    Docsting for maxmin

--- max_min.py
+++ max_min.py
@@ -1,5 +1,11 @@
 import sys
 def method_1(arr):
+    """
+    This method iterates through the given array to find the maximum and minimum values.
+    It initializes `int_min` and `int_max` to store the minimum and maximum values respectively.
+    The method compares each element with `int_min` and `int_max` and updates them accordingly.
+    Finally, it prints out the maximum and minimum values found in the array.
+    """
     int_min = sys.maxsize
     int_max = -sys.maxsize-1
     for i in arr:
@@ -10,6 +16,11 @@ def method_1(arr):
     print("Maximum: " + str(int_max) + " | Minimum: " + str(int_min))
     
 def method_2(arr):
+    """
+    This method finds the maximum and minimum values in the given array using built-in Python functions.
+    It utilizes the `max()` function to find the maximum value and the `min()` function to find the minimum value.
+    The method then prints out these values.
+    """
     print("Maximum: " + str(max(arr)) + " | Minimum: " + str(min(arr)))
     
 if __name__ == "__main__":
